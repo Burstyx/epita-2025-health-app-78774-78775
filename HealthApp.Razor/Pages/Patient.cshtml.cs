88ca3e65c6ebc2f3cae2c480032f25ba718a852e
Toni Gomez <toni.bursty@gmail.com>
2025-03-11 11:45:48 +0000

    add Patient page and update navigation

---
+++ HealthApp.Razor/Pages/Patient.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HealthApp.Razor.Pages;
+
+[Authorize(Roles = "Patient")]
+public class Patient : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
